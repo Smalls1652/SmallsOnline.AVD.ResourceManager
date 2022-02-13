44b7879f8ccc5aa668f2047cd2c3314c7198e1c0
Timothy Small <smalls.online@icloud.com>
2022-02-13 17:10:42 -0500

    Updated the nullability of some properties
    Affects the following properties: ObjectId and ResourceId.

--- models/azure/desktop-virtualization/interfaces/ISessionHostProperties.cs
+++ models/azure/desktop-virtualization/interfaces/ISessionHostProperties.cs
@@ -2,8 +2,8 @@ namespace SmallsOnline.AVD.ResourceManager.Models.Azure.DesktopVirtualization;
 
 public interface ISessionHostProperties
 {
-    string? ObjectId { get; set; }
-    string? ResourceId { get; set; }
+    string ObjectId { get; set; }
+    string ResourceId { get; set; }
     int? Sessions { get; set; }
     string? Status { get; set; }
     bool AllowNewSession { get; set; }
