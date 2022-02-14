635fb0e389bb3587abc0bea9fb0e1ca8c17583ac
Timothy Small <smalls.online@icloud.com>
2022-02-14 13:46:22 -0500

    Add auto configure monitored hostpools (#11)
    * Added models for HostPool
    
    * Added GetHostPools() method to AzureApiService
    
    * Added AddHostPool() method to CosmosDbService
    
    * Added EvaluateHostPoolsToMonitor_Timer function

---
+++ models/azure/desktop-virtualization/HostPool.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+namespace SmallsOnline.AVD.ResourceManager.Models.Azure.DesktopVirtualization;
+
+/// <summary>
+/// Details, retrieved from the Azure API, about a hostpool.
+/// </summary>
+public class HostPool : IHostPool
+{
+    public HostPool() {}
+
+    /// <summary>
+    /// The name of the hostpool.
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// The resource id of the hostpool.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = default!;
+
+    /// <summary>
+    /// The Azure datacenter region the hostpool is located at.
+    /// </summary>
+    [JsonPropertyName("location")]
+    public string Location { get; set; } = default!;
+
+    /// <summary>
+    /// Additional properties of the hostpool.
+    /// </summary>
+    [JsonPropertyName("properties")]
+    public HostPoolProperties Properties { get; set; } = default!;
+}
---
+++ models/azure/desktop-virtualization/HostPoolProperties.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+
+namespace SmallsOnline.AVD.ResourceManager.Models.Azure.DesktopVirtualization;
+
+/// <summary>
+/// Additional properties of the hostpool.
+/// </summary>
+public class HostPoolProperties : IHostPoolProperties
+{
+    public HostPoolProperties() {}
+
+    /// <summary>
+    /// The object id of the hostpool.
+    /// </summary>
+    [JsonPropertyName("objectId")]
+    public string ObjectId { get; set; } = default!;
+
+    /// <summary>
+    /// The type of hostpool it is.
+    /// </summary>
+    [JsonPropertyName("hostPoolType")]
+    public string HostPoolType { get; set; } = default!;
+}
